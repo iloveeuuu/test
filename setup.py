6df6b36c6c1af53de8b94cc547f46832f8088fd1
iloveeuuu <139212529+iloveeuuu@users.noreply.github.com>
2024-08-21 23:51:16 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,9 +4,9 @@ setup(
     name="takeover",
     version="0.1",
     description="Sub-Domain TakeOver Vulnerability Scanner",
-    url="https://github.com/m4ll0k/takeover",
+    url="https://github.com/iloveeuuu/test.git",
     author="m4ll0k",
-    author_email="m4ll0k@protonmail.com",
+    author_email="mrleechunsan@hotmail.com",
     license="MIT",
     scripts=["takeover.py"],
     install_requires=[
